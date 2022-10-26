b5d89fe61d325e7a2ed281c01001ea12ed7591ac
sahil thakur <st7378539838@gmail.com>
2022-10-26 19:26:01 +0530

    initial

--- App.js
+++ App.js
@@ -1,10 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View ,Button, TextInput} from 'react-native';
+import * as firebase from "firebase/app"
+import {getDatabase,ref,onValue ,update,set} from "firebase/database"
 
+import { firebaseConfig } from "./config"
 export default function App() {
+  const [moisture, setMoiture] = useState(0); 
+  const [isoff, setisoff] = useState(true); 
+  const [speed, setspeed] = useState(0)
+  const [buttontext, setbuttonstext] = useState("ON");
+  // if(!firebase.app.length)
+  const app = firebase.initializeApp(firebaseConfig)
+  const db = getDatabase(app)
+  const handleChange = (event) =>
+  {
+    setspeed(event.target.value);
+  }
+  const handleClick = () => {
+   
+    if(isoff)
+    { setisoff(false)
+      setbuttonstext("OFF")
+      set(ref(db, "/Pump Status"),false)
+      // update(ref(db, '/'), {
+        // Pump\status
+      // });
+    }
+    else 
+    {
+      setisoff(true)
+      set(ref(db, "/Pump Status"), true)
+
+      // setbuttonstext("ON")
+    }
+  }
+  useEffect(() => {
+     onValue(ref(db, '/Moisture'), querySnapShot => {
+       let data = querySnapShot.val() || {};
+      //  console.log(data)
+     setMoiture(data);
+    
+     })
+    onValue(ref(db, '/Pump Status'), querySnapShot => {
+      let data = querySnapShot.val() || {};
+      // setisoff(data);
+
+      console.log(data);
+      if (data={}) {
+        setisoff(false)
+        setbuttonstext("OFF")
+        // update(ref(db, '/'), {
+        // Pump\status
+        // });
+      }
+      else {
+        setisoff(true)
+        // set(ref(db, "/Pump Status"), true)
+
+        setbuttonstext("ON")
+      }
+    })
+    // console.log(moisturevalue)
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text><h1>Smart Irrigation </h1></Text>
+      <Text>Moiture:- {moisture} </Text>
+      <Button onPress={handleClick} title={buttontext} style={{
+      }}></Button>
+
+      <TextInput keyboardType='numeric' name  = "motarspeed " value = {speed} placeholder='Enter speed of motar' onChange={handleChange}/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-WlfsLoJYQuQrzWchLSu5VnWkQn9KK1w",
+    authDomain: "smart-irrigation-d65e8.firebaseapp.com",
+    databaseURL: "https://smart-irrigation-d65e8-default-rtdb.firebaseio.com",
+    projectId: "smart-irrigation-d65e8",
+    storageBucket: "smart-irrigation-d65e8.appspot.com",
+    messagingSenderId: "719689605654",
+    appId: "1:719689605654:web:76aa4449314955f34c8b37",
+    measurementId: "G-784DDFB1ZG"
+};
